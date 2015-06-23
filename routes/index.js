161e16edb8a850245b1c3773b4a935bda246b86a
Miguel <kastedo@users.noreply.github.com>
2015-06-23 18:01:52 +0200

    Añadida ruta /author a la navegación

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,11 @@ router.get('/', function(req, res) {
 });
 
 router.get('/quizes/question', quizController.question);
-router.get('quizes/answer', quizController.answer);
+router.get('/quizes/answer', quizController.answer);
+
+/* GET home page. */
+router.get('/author', function(req, res) {
+  res.render('author');
+});
 
 module.exports = router;
